5111e77df34f7a190677e41f17c2866edf659d9b
assanmboge <mbogeassan77@gmail.com>
2020-03-29 15:50:51 +0300

    Initial commit of my product shop api after rm node_modules

--- shop-frontend/src/components/product-list.js
+++ shop-frontend/src/components/product-list.js
@@ -13,7 +13,7 @@ class ProductList extends Component {
 
   componentDidMount(){
     
-    axios.get("https://productshopapi.herokuapp.com//products/")
+    axios.get("https://productshopapi.herokuapp.com/products/")
     .then(res => {
         this.setState({productCollection: res.data.createdProduct.products});
 
